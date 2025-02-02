eac7cfa1cecd9ec8bacb0492c07610d0b4e14a29
Sally Ramadhani <fadhliilhami1401@gmail.com>
2025-02-02 19:31:38 +0700

    Update

---
+++ autorun.sh
@@ -0,0 +1,4 @@
+sudo apt install ukui-session-manager ukui-menu ukui-control-center ukui-settings-daemon ukui-window-switch ukui-media ukui-power-manager peony ukui-themes ukui-greeter kylin-display-switch ukui-sidebar qt5-ukui-platformtheme kylin-nm ukui-wallpapers
+
+gsettings set org.mate.interface gtk-theme 'ukui-black'
+gsettings set org.ukui.style icon-theme-name 'ukui-icon-theme-default'
